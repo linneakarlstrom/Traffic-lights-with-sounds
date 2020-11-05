5837da9ede4113609b25abc62b89faf8ebb562e4
Linnea Karlström <linnea.karlstrom@skola.botkyrka.se>
2020-11-05 10:19:58 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+
+// röd lampa pin 13 (50), grön lampa pin 7 (90), gul lampa pin 3 (70)
+const int buzzer = 8;
+
+void setup() {
+// put your setup code here, to run once:
+  pinMode(buzzer, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(13,OUTPUT);
+  pinMode(3,OUTPUT);
+}
+
+void loop() {
+// put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH); //röd lampa lyser
+  tone(buzzer, 50); // ljudet för röd lampa sätts på
+  delay(2500);
+
+  digitalWrite(3, HIGH); // gul lampa lyser
+  noTone(buzzer);
+  tone(buzzer, 70); // ljudet för gul lampa sätts på
+  delay(2500);
+  noTone(buzzer); // ljudet slutar
+
+  digitalWrite(3, LOW); // gul lampa släcks
+  digitalWrite(13, LOW); // röd lampa släcks
+
+  digitalWrite(7, HIGH); //grön lampa lyser
+  noTone(buzzer);
+  tone(buzzer, 90); //ljudet för grön lampa sätts på
+  delay(2500);
+  digitalWrite(7, LOW); // grön lampa släcks
+  noTone(buzzer); // ljudet slutar
+  
+  digitalWrite(3, HIGH); // gul lampa sätts på
+  noTone(buzzer);
+  tone(buzzer, 70);
+  delay(2500);
+  digitalWrite(3, LOW);
+  noTone(buzzer);
+}
+
